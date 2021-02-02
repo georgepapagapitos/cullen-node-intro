42382249d670a75b52c1e1d56442a96844779fc4
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-02 09:29:43 -0600

    some testing

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log('js');
+
+let age = 30;
+
+if (age > 23) {
+  console.log('you are old man');
+}
+
+function birthday() {
+  age++;
+  return age;
+}
+
+console.log(`happy birthday to you, you are ${birthday()}`);
+
+let cullenites = ['Michael', 'Dusty', 'George', 'Vada'];
+
+for (let student of cullenites) {
+  console.log(`welcome to node week, ${student}`);
+}
+
+$(document).ready(() => {
+  $(body).append('<h1>Hello World</h1>');
+});
